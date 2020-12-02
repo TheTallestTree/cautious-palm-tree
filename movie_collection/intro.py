f035f4e2cf3f04c9029a5ef8d98d15d75e6ce927
TheTallestTree <hoetinger@gmail.com>
2020-12-01 23:59:51 -0500

    started python course

---
+++ movie_collection/intro.py
@@ -0,0 +1,23 @@
+age = 30
+# print(age)
+
+age = 40
+# print(age)
+
+friend_age_2 = 23
+countries_visited = 90
+
+PI = 3.14159
+# Uppercase when you enver want to change the variable
+
+age = 35   # integer
+PI = 3.14159   # float
+
+maths_operation = 1 + 3 * 4 / 2 - 2  # follow order of operations
+# print(maths_operation)
+
+float_division = 12 / 3
+# print(float_division)
+
+integer_division = 12 // 3
+# print(integer_division) #integer division just removes everything after the decimal place. Does NOT round
